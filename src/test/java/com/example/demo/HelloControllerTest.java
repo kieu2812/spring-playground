bf1e5e9bf8d8c9bfa53d74bd4fa80d08bfa77637
kieu2812 <kieu2812@gmail.com>
2021-05-10 19:56:01 -0400

    initial commit

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import  org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testHello() throws Exception {
+        RequestBuilder request  = MockMvcRequestBuilders.get("/hello");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+
+    }
+
+
+
+
+}
