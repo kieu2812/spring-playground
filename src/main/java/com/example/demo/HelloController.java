7eb04634f09a5defb3097ba5c7dd0cd0b7976ead
kieu2812 <kieu2812@gmail.com>
2021-05-11 18:48:31 -0400

    build math/pi API

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -9,5 +8,23 @@ public class HelloController {
     @GetMapping("/hello")
     public String getGreeting(){
         return "hello";
+    }
+    @GetMapping("/greeting/{first}/{last}")
+    public String greetingByParams(@PathVariable String first, @PathVariable String last){
+
+        return "Greeting by params: " +first + last;
+    }
+    @GetMapping("/greeting")
+    public String greetingByQuery(@RequestParam String first, @RequestParam String last){
+        return "Greeting by queries: " +first + last;
+    }
+    @PostMapping("/greeting")
+    public String greetingByFormData(@RequestParam String first, @RequestParam String last){
+        return "Greeting by form data: " +first + last;
+    }
+
+    @GetMapping("/math/pi")
+    public String getPI(){
+        return "3.141592653589793";
     }
 }
--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
-import  org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,7 +24,42 @@ public class HelloControllerTest {
 
     }
 
+    @Test
+    public void testGreetingByParams() throws Exception {
+        String first="kieu", last="nguyen";
+        RequestBuilder request  = MockMvcRequestBuilders.get("/greeting/"+ first +"/"+ last);
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Greeting by params: " +first + last));
 
+    }
 
+    @Test
+    public void testGreetingByQueries() throws Exception {
+        String first="kieu", last="nguyen";
+        RequestBuilder request  = MockMvcRequestBuilders.get("/greeting?first="+ first +"&last="+ last);
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Greeting by queries: " +first + last));
 
+    }
+    @Test
+    public void testGreetingByFormData() throws Exception {
+        String first="kieu", last="nguyen";
+        RequestBuilder request  = MockMvcRequestBuilders.post("/greeting")
+                .param("first", first)
+                .param("last", last);
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Greeting by form data: " +first + last));
+
+    }
+
+    @Test
+    public void testGetPI() throws Exception{
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/pi");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("3.141592653589793"));
+    }
 }
