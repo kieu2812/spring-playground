5b2560706c7d3b4edf0364fb23b9aba746ced6ab
kieu2812 <kieu2812@gmail.com>
2021-05-12 19:51:12 -0400

    Calculate Area

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,8 +9,8 @@ import java.util.stream.IntStream;
 @RestController
 public class HelloController {
 
-    @Autowired
-    private MathService mathService;
+//    @Autowired
+//    private MathService mathService;
 
     @GetMapping("/hello")
     public String getGreeting(){
@@ -37,19 +37,83 @@ public class HelloController {
     @GetMapping("/math/calculate")
     public String calculateNumbers(@RequestParam(value="operation", required = false, defaultValue = "add") String operation,
                                    @RequestParam("x") int x, @RequestParam("y") int y){
-        return mathService.operateNumbers(operation, x, y);
+       // return mathService.operateNumbers(operation, x, y);
+        return operateNumbers(operation, x, y);
     }
 
     @GetMapping("/math/sum")
-    public String sumNumbers(@RequestParam("n") List<Integer> numbers){
-        return mathService.sumNumbers(numbers);
+    public String sumListOfNumbers(@RequestParam("n") List<Integer> numbers){
+       // return mathService.sumNumbers(numbers);
+        return sumNumbers(numbers);
 
     }
 
 
-    @GetMapping("/math/volume/{length}/{width}/{height}")
-    public String sumNumbers(@PathVariable("length") int length, @PathVariable("width") int width, @PathVariable("height") int height ){
-        return mathService.calculateVolumn(length, width, height);
+    @PostMapping("/math/volume/{length}/{width}/{height}")
+    public String calculateVolumeOfShape(@PathVariable("length") int length, @PathVariable("width") int width, @PathVariable("height") int height ){
+       // return mathService.calculateVolumn(length, width, height);
+        return calculateVolume(length, width, height);
 
+    }
+
+    @PostMapping("/math/area")
+    public String calculateAreaOfShape(@RequestParam(value = "type", required = true) String type, @RequestParam(value = "radius", required = false) Integer radius,
+                                @RequestParam(value = "width", required = false) Integer width, @RequestParam(value = "height", required = false) Integer height){
+        // return mathService.calculateVolumn(length, width, height);
+        String result=null;
+        if("rectangle".equalsIgnoreCase(type)){
+            result = calculateRectangleArea( width, height);
+        }
+        else if("circle".equalsIgnoreCase(type)){
+            result = calculateCircleArea( radius);
+        }
+        else
+            result ="Invalid";
+        return result;
+
+    }
+
+    private String calculateRectangleArea(int width, int height) {
+        return String.format("Area of %dx%d rectangle is %d", width, height, width*height);
+    }
+    private String calculateCircleArea(int radius) {
+        float PI= 3.141592653589793f;
+        return String.format("Area of a circle with radius of %d is %f", radius, radius* PI);
+    }
+
+    public String operateNumbers(String operation, int x, int y){
+
+        String result=null;
+        switch (operation){
+            case "multiply":
+                result= x +" * " + y + " = " + (x*y);
+                break;
+            case "divide":
+                result= x +" / " + y + " = " + (x/y);
+                break;
+            case "subtract":
+                result= x +" - " + y + " = " + (x-y);
+                break;
+            default:
+                result= x +" + " + y + " = " + (x+y);
+                break;
+        }
+        return result;
+
+    }
+    public String sumNumbers(List<Integer> numbers){
+        StringBuilder builder = new StringBuilder();
+        long sum = numbers.stream().mapToInt( e-> {
+            builder.append(" + " + e);
+            return Integer.valueOf(e);
+        }).sum();
+        builder.append(" = " + sum);
+        return builder.toString().isEmpty() ? "" : builder.toString().substring(3);
+
+    }
+
+    public String calculateVolume(int length, int width, int height) {
+        int volumn = length * width * height;
+        return String.format("The volume of a %dx%dx%d rectangle is %d", length, width, height, volumn);
     }
 }
--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -22,8 +22,8 @@ public class HelloControllerTest {
     @Autowired
     MockMvc mvc;
 
-    @MockBean
-    private MathService mathService;
+//    @MockBean
+//    private MathService mathService;
 
     @Test
     public void testHello() throws Exception {
@@ -84,73 +84,114 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(x +" + " + y + " = " + (x+y)));
     }
-//    @Test
-//    public void testAddDefault() throws  Exception{
-//        int x= 5, y=7;
-//        RequestBuilder request = get("/math/calculate")
-//                .param("x", String.valueOf(x))
-//                .param("y",  String.valueOf(y));
-//        this.mvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(x +" + " + y + " = " + (x+y)));
-//    }
-//    @Test
-//    public void testSubtractOperations() throws  Exception{
-//        int x= 5, y=7;
-//
-//        RequestBuilder request = get("/math/calculate")
-//                .param("x", String.valueOf(x))
-//                .param("y",  String.valueOf(y))
-//                .param("operation", "subtract");
-//        this.mvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(x +" - " + y + " = " + (x-y)));
-//    }
-//
-//    @Test
-//    public void testMultiplyOperations() throws  Exception{
-//        int x= 5, y=7;
-//        RequestBuilder request = get("/math/calculate")
-//                .param("x", String.valueOf(x))
-//                .param("y",  String.valueOf(y))
-//                .param("operation", "multiply");
-//        this.mvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(x +" * " + y + " = " + (x*y)));
-//    }
-//    @Test
-//    public void testDivideOperations() throws  Exception{
-//        int x= 5, y=7;
-//        RequestBuilder request = get("/math/calculate")
-//                .param("x", String.valueOf(x))
-//                .param("y",  String.valueOf(y))
-//                .param("operation", "divide");
-//        this.mvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(x +" / " + y + " = " + (x/y)));
-//    }
-//    @Test
-//    public void testSumNumbers() throws Exception{
-//        int x=5, y=7, z=9;
-//        RequestBuilder request = get("/math/sum")
-//                .param("n", ""+ x)
-//                .param("n", ""+ y)
-//                .param("n", ""+ z);
-//        this.mvc.perform(request)
-//                .andExpect(status().isOk())
-//                .andExpect(content().string("5 + 7 + 9 = 21"));
-//    }
-        @Test
+    @Test
+    public void testAddDefault() throws  Exception{
+        int x= 5, y=7;
+        RequestBuilder request = get("/math/calculate")
+                .param("x", String.valueOf(x))
+                .param("y",  String.valueOf(y));
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(x +" + " + y + " = " + (x+y)));
+    }
+    @Test
+    public void testSubtractOperations() throws  Exception{
+        int x= 5, y=7;
+
+        RequestBuilder request = get("/math/calculate")
+                .param("x", String.valueOf(x))
+                .param("y",  String.valueOf(y))
+                .param("operation", "subtract");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(x +" - " + y + " = " + (x-y)));
+    }
+
+    @Test
+    public void testMultiplyOperations() throws  Exception{
+        int x= 5, y=7;
+        RequestBuilder request = get("/math/calculate")
+                .param("x", String.valueOf(x))
+                .param("y",  String.valueOf(y))
+                .param("operation", "multiply");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(x +" * " + y + " = " + (x*y)));
+    }
+    @Test
+    public void testDivideOperations() throws  Exception{
+        int x= 5, y=7;
+        RequestBuilder request = get("/math/calculate")
+                .param("x", String.valueOf(x))
+                .param("y",  String.valueOf(y))
+                .param("operation", "divide");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(x +" / " + y + " = " + (x/y)));
+    }
+    @Test
+    public void testSumNumbers() throws Exception{
+        int x=5, y=7, z=9;
+        RequestBuilder request = get("/math/sum")
+                .param("n", ""+ x)
+                .param("n", ""+ y)
+                .param("n", ""+ z);
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("5 + 7 + 9 = 21"));
+    }
+    @Test
     public void testVolumn() throws Exception{
         int length=5, width=7, height=9;
         int volumn =  length * width * height;
-        RequestBuilder request = get("/math/volumn")
-                .param("length", ""+ length)
-                .param("width", ""+ width)
-                .param("height", ""+ height);
+        RequestBuilder request = post("/math/volume/{length}/{width}/{height}", length, width, height);
+        System.out.println(request.toString());
+//                .param("length", ""+ length)
+//                .param("width", ""+ width)
+//                .param("height", ""+ height);
         this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().string(String.format("The volume of a %dx%dx%d rectangle is %d", length, width, height, volumn)));
     }
+    @Test
+    public void testCircleArea() throws Exception{
+        int radius=5;
+        float PI=3.141592653589793f;
+        RequestBuilder request = post("/math/area")
+                .param("type", "circle")
+                .param("radius", String.valueOf(radius));
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(String.format("Area of a circle with radius of %d is %f", radius, radius* PI)));
 
+    }
+    @Test
+    public void testRectangleArea() throws Exception{
+        int width=5;
+        int height =10;
+        float PI=3.141592653589793f;
+        RequestBuilder request = post("/math/area")
+                .param("type", "rectangle")
+                .param("width", String.valueOf(width))
+                .param("height", String.valueOf(height));
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(String.format("Area of %dx%d rectangle is %d", width, height, width*height)));
+
+    }
+    @Test
+    public void testInvalidArea() throws Exception{
+        int width=5;
+        int height =10;
+        float PI=3.141592653589793f;
+        RequestBuilder request = post("/math/area")
+                .param("type", "square")
+                .param("width", String.valueOf(width))
+                .param("height", String.valueOf(height));
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Invalid"));
+
+    }
 }
+
